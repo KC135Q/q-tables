0e9d89ecf6c2966762733a4bd902d1edcf29a7f2
Dan K <KC135Q@users.noreply.github.com>
2017-10-20 20:39:56 -0400

    Working MVP

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var path = require('path');
 
 var PORT = process.env.PORT || 3000;
 
 // placeholders for app.use boilerplate
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set a basic table reservation
+var tables = [{
+    "name": "Dan",
+    "email": "d.a.kaltenbaugh@gmail.com",
+    "id": "DannyK",
+    "phone": "618-579-8085"
+}];
+
+var waitList = [];
 
 // Set get root route
 app.get('/', function(req, res) {
    res.send("<h1>Welcome to Q's Tables.");
+});
+
+app.get('/home', function(req, res) {
+   res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/wait-list', function(req, res) {
+    res.send("<h1>Wait List</h1>");
+});
+
+app.get('/api/tables', function(req, res) {
+   res.json(tables);
+});
+
+app.get('/api/wait-list', function(req, res) {
+    res.json(waitList);
+});
+
+app.post('/api/reserve', function(req, res) {
+   var newReservation = req.body;
+   if (tables.length < 5) {
+       tables.push(newReservation);
+       res.redirect("/tables");
+   } else {
+       waitList.push(newReservation);
+       res.redirect("/wait-list");
+   }
 });
 
 app.listen(PORT, function() {
